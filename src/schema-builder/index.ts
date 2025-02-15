cc33b0528e13c09089ff2c86d4bcd1ccc4107011
Ben <ben@zfben.com>
2025-02-15 06:21:30 +0000

    feat: refactor SchemaBuilder to use a unified changes array for table operations

--- src/schema-builder/index.ts
+++ src/schema-builder/index.ts
@@ -4,8 +4,7 @@ import { TableBuilder } from "./table-builder"
 
 export class SchemaBuilder {
   private client: Client
-  private tables: Map<string, TableBuilder> = new Map()
-  private raws: string[] = []
+  private changes: (string | TableBuilder)[] = []
 
   constructor(client: Client) {
     this.client = client
@@ -14,35 +13,37 @@ export class SchemaBuilder {
   createTable(tableName: string, callback: (table: TableBuilder) => void) {
     const builder = new TableBuilder(tableName, 'create')
     callback(builder)
-    this.tables.set(tableName, builder)
+    this.changes.push(builder)
     return this
   }
 
   alterTable(tableName: string, callback: (table: TableBuilder) => void) {
-    const builder = this.tables.get(tableName) || new TableBuilder(tableName, 'alter')
+    const builder = new TableBuilder(tableName, 'alter')
     callback(builder)
-    this.tables.set(tableName, builder)
+    this.changes.push(builder)
     return this
   }
 
   renameTable(oldTableName: string, newTableName: string) {
-    this.raws.push(`alter table ${escapeIdentifier(oldTableName)} rename to ${escapeIdentifier(newTableName)};`)
+    this.changes.push(`alter table ${escapeIdentifier(oldTableName)} rename to ${escapeIdentifier(newTableName)};`)
     return this
   }
 
   dropTable(tableName: string) {
-    this.raws.push(`drop table ${escapeIdentifier(tableName)};`)
+    this.changes.push(`drop table ${escapeIdentifier(tableName)};`)
     return this
   }
 
   toSQL(): string[] {
     const statements: string[] = []
 
-    for (const builder of this.tables.values())
-      statements.push(...builder.toSQL())
-
-    if (this.raws.length)
-      statements.push(...this.raws)
+    for (const builder of this.changes) {
+      if (typeof builder === 'string') {
+        statements.push(builder)
+      } else {
+        statements.push(...builder.toSQL())
+      }
+    }
 
     return statements
   }
@@ -54,6 +55,6 @@ export class SchemaBuilder {
 
     await this.client.postgres.begin(async trx => await trx.call(trx, createTemplateStringsArray(statements.join("\n"))).simple())
 
-    this.tables.clear()
+    this.changes = []
   }
 }
